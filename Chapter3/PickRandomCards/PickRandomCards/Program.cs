7d9536d4e553872439e8f9ef5a73ae4130d05b61
jromcevic16@gmail.com <jromcevic16@gmail.com>
2025-01-18 05:00:52 +0100

    Initial commit chapeter 3

---
+++ Chapter3/PickRandomCards/PickRandomCards/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PickRandomCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isNumber = false;
+            Console.Write("Enter the number of cards to pick: ");
+            while (!isNumber)
+            {
+                string line = Console.ReadLine();
+                if (int.TryParse(line, out int numberOfCards))
+                {
+                    isNumber = true;
+                    foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                    {
+                        Console.WriteLine(card);
+                    }
+                }
+                else
+                {
+                    Console.Write("Please Enter a valide number: ");
+                }
+            }
+        }
+    }
+}
