b1ec786100e9cca0f3783d65f1a4dac49e94eb68
jromcevic16@gmail.com <jromcevic16@gmail.com>
2025-02-20 01:28:44 +0100

    chapeter 5 SwordDamageWpf

---
+++ Chapter5/RollForDamage/RollForDamage/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace RollForDamage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            SwordDamage swordDamage = new SwordDamage();
+            while (true)
+            {
+                Console.Write("0 for no magic/flaming, 1 for magic, 2 for flaming, 3 for both, aythingelse to quit:");
+                char key = Console.ReadKey(false).KeyChar;
+                if (key != '1' && key != '2' && key != '3' && key != '4') return;
+                int damage = 0;
+                for (int i = 0; i < 3; i++)
+                {
+                    damage += random.Next(1, 7);
+                }
+                swordDamage.Roll = damage;
+                swordDamage.SetMagic(key == '1' || key == '3');
+                swordDamage.SetFlaming(key == '2' || key == '3');
+                Console.WriteLine("\nRolled "+swordDamage.Roll+" for "+swordDamage.Damage+" HP");
+
+            }
+        }
+    }
+}
---
+++ Chapter5/SwordDamageWpf/SwordDamageWpf/SwordDamage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace SwordDamageWpf
+{
+    public class SwordDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+        public int Roll;
+        private decimal magicMultiplier = 1M;
+        private int flamingDamage = 0;
+        public int Damage;
+        private void CalculateDamage()
+        {
+            Damage = (int)(Roll * magicMultiplier) + BASE_DAMAGE + flamingDamage;
+            Debug.WriteLine($"CalculatedDamage finished: {Damage} (roll: {Roll})");
+        }
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)
+            {
+                magicMultiplier = 1.75M;
+            }
+            else
+            {
+                magicMultiplier = 1M;
+            }
+            CalculateDamage();
+            Debug.WriteLine($"SetMagic finished: {Damage} (roll: {Roll})");
+        }
+        public void SetFlaming(bool isFlaming)
+        {
+            CalculateDamage();
+            if (isFlaming)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+            Debug.WriteLine($"SetFlaming finished: {Damage} (roll: {Roll})");
+        }
+    }
+}
