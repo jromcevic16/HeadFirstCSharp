5da716bf5daa447945f74aa2610f0f66c7e355e0
jromcevic16@gmail.com <jromcevic16@gmail.com>
2025-02-20 04:32:55 +0100

    chapeter 5 Paintball Gun

---
+++ Chapter5/Paintball/Paintball/PaintballGun.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paintball
+{
+    class PaintballGun
+    {
+        public int MagazineSize { get; private set; } = 16;
+        private int balls;
+        public int Balls 
+        {
+            get { return balls; }
+            set
+            {
+                if (value > 0)
+                    balls = value;
+                Reload();
+            }
+        }
+
+        public PaintballGun(int balls, int magazineSize, bool loaded)
+        {
+            this.balls = balls;
+            MagazineSize = magazineSize;
+            if (!loaded) Reload();
+        }
+
+        public int BallsLoaded { get; private set; }
+
+        public bool IsEmpty() { return BallsLoaded == 0; }
+        public void Reload()
+        {
+            if (balls > MagazineSize)
+                BallsLoaded = MagazineSize;
+            else
+                BallsLoaded = Balls;
+        }
+        public bool Shoot()
+        {
+            if (BallsLoaded == 0) return false;
+            BallsLoaded--;
+            balls--;
+            return true;
+        }
+    }
+}
