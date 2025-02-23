a537fed077d988d7d1d1b0402c5163f1476d11bf
jromcevic16@gmail.com <jromcevic16@gmail.com>
2025-02-23 01:10:06 +0100

    chapeter 6 Safe and Steal

---
+++ Chapter6/Safe and Steal/Safe and Steal/Locksmith.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Safe_and_Steal
+{
+    internal class Locksmith
+    {
+        public string Combination { private get; set; }
+        public void OpenSafe(Safe safe, SafeOwner owner)
+        {
+            safe.PickLock(this);
+            string safeContents = safe.Open(Combination);
+            ReturnContents(safeContents, owner);
+        }
+        protected virtual void ReturnContents(string safeContents, SafeOwner owner)
+        {
+            owner.ReceiveContents(safeContents);
+        }
+    }
+}
---
+++ Chapter6/Safe and Steal/Safe and Steal/SafeOwner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Safe_and_Steal
+{
+    internal class SafeOwner
+    {
+        private string valuables = "";
+        public void ReceiveContents(string safeContetns)
+        {
+            valuables = safeContetns;
+            Console.WriteLine($"Thank you for returning my {valuables}!");
+        }
+    }
+}
