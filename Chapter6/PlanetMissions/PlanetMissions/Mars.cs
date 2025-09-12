a20eba7b4e09c443fdf65329b99ce29a7d58ac4a
jromcevic16@gmail.com <jromcevic16@gmail.com>
2025-09-12 14:33:24 +0200

    chapeter 6 PlanetMissions

---
+++ Chapter6/PlanetMissions/PlanetMissions/Mars.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetMissions
+{
+    internal class Mars : PlanetMission
+    {
+        public Mars()
+        {
+            kmToPlanet = 92000000;
+            fuelPerKm = 1.73f;
+            kmPerHour = 37000;
+        }
+    }
+}
---
+++ Chapter6/PlanetMissions/PlanetMissions/PlanetMission.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetMissions
+{
+    internal abstract class PlanetMission
+    {
+        protected float fuelPerKm;
+        protected long kmPerHour;
+        protected long kmToPlanet;
+
+        public string MissionInfo()
+        {
+            long fule = (long)(kmToPlanet * fuelPerKm);
+            long time = kmToPlanet / kmPerHour;
+            return $"We'll burn {fule} units of guel in {time} hours";
+        }
+    }
+}
