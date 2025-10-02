b2a7b575556619b42c0a1558fa14d1993944f2b1
jromcevic16@gmail.com <jromcevic16@gmail.com>
2025-10-03 00:35:55 +0200

    chapeter 8 Cards

---
+++ Chapter8/Cards/Cards/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Cards
+{
+    internal class Program
+    {
+
+        private static Random random = new Random();
+        static void Main(string[] args)
+        {
+            Card card = new Card((Values)random.Next(1, 14), (Suits)random.Next(4));
+            Console.WriteLine(card.Name);
+        }
+    }
+}
