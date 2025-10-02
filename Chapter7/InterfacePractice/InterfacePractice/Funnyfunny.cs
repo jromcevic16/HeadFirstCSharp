04bd9acf3a3dbfb645615e6c225aba0cbdb9d786
jromcevic16@gmail.com <jromcevic16@gmail.com>
2025-10-02 15:55:41 +0200

    chapeter 7 interface practesing

---
+++ Chapter7/InterfacePractice/InterfacePractice/Funnyfunny.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacePractice
+{
+    internal class Funnyfunny : IClown
+    {
+        private readonly string funnyThingIHave;
+        public string FunnyThingIHave { get { return funnyThingIHave; } }
+        public Funnyfunny(string funnyThingIHave)
+        {
+            this.funnyThingIHave = funnyThingIHave;
+        }
+        public void Honk()
+        {
+            Console.WriteLine($"Hi kids! I have a {funnyThingIHave}.");
+        }
+    }
+}
---
+++ Chapter7/InterfacePractice/InterfacePractice/IScaryClown.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacePractice
+{
+    internal interface IScaryClown : IClown
+    {
+        string ScaryThingIHave { get; }
+        void ScareLittleChildren();
+        void ScareAdults()
+        {
+            Console.WriteLine($@"I am an ancient evil that will hunt your dreamns.
+behold my terifying necklace with {random.Next(4, 10)} of my last victim's fingers.
+
+Oh, also, beforei forget...");
+            ScareLittleChildren();
+        }
+    }
+}
