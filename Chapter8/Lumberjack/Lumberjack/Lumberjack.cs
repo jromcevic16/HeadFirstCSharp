0aacc0ac6d44de6dd176ca07d9c80cb5047f2ecc
jromcevic16@gmail.com <jromcevic16@gmail.com>
2025-10-06 23:00:58 +0200

    chapeter 8 lumberjack

---
+++ Chapter8/Lumberjack/Lumberjack/Lumberjack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lumberjack
+{
+    internal class Lumberjack
+    {
+        public string Name { get; private set; }
+        private Stack<Flapjack> flapjackStack = new Stack<Flapjack>();
+        public Lumberjack(string name)
+        {
+            Name = name;
+        }
+        public void TakeFlapjack(Flapjack flapjack)
+        {
+            flapjackStack.Push(flapjack);
+        }
+        public void EatFlapjack()
+        {
+            Console.WriteLine($"{Name} ate a {flapjackStack.Pop()} flapjack");
+        }
+        public void EatAllFlapjacks()
+        {
+            while(flapjackStack.Count > 0)
+            {
+                EatFlapjack();
+            }
+        }
+    }
+}
---
+++ Chapter8/Lumberjack/Lumberjack/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Lumberjack
+{
+    internal class Program
+    {
+        public static Random Random = new Random();
+        static void Main(string[] args)
+        {
+            Queue<Lumberjack> lumberjacks = new Queue<Lumberjack>();
+            Console.Write("First lumberjack's name: ");
+            string name = Console.ReadLine();
+            while (name != "")
+            {
+                Console.Write("Number of flapjacks: ");
+                Lumberjack lumberjack = new Lumberjack(name);
+                if (int.TryParse(Console.ReadLine(), out int flapjeckNumber))
+                    for (int i = 0; i < flapjeckNumber; i++)
+                        lumberjack.TakeFlapjack((Flapjack)Random.Next(4));
+                lumberjacks.Enqueue(lumberjack);
+                Console.Write("Next lumberjack's name (blank to end): ");
+                name = Console.ReadLine();
+            }
+            foreach(Lumberjack lj in lumberjacks)
+            {
+                Console.WriteLine($"{lj.Name} is eating flapjacks");
+                lj.EatAllFlapjacks();
+            }
+        }
+    }
+}
