d1274fe20fdc1630d490e648bb832ea49270f4de
jromcevic16@gmail.com <jromcevic16@gmail.com>
2025-10-09 14:46:17 +0200

    chapeter 9 comics

---
+++ Chapter9/Comics/Comics/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Comics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> mostExpensive =
+                from comic in Comic.Catalog
+                where Comic.Prices[comic.Issue] > 500
+                orderby Comic.Prices[comic.Issue] descending
+                select $"{comic} is worth {Comic.Prices[comic.Issue]:c}";
+            foreach (string com in mostExpensive)
+            {
+                Console.WriteLine(com);
+            }
+        }
+    }
+}
