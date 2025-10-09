612bd247bf9a500558224e056fcdd808f56b2c8c
jromcevic16@gmail.com <jromcevic16@gmail.com>
2025-10-09 14:45:54 +0200

    chapeter 9 clown adwance

---
+++ Chapter9/Clown2.0/Clown2.0/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Clown2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TallGuy tallGuy = new TallGuy() { Height = 76, Name = "Jimmy"};
+            tallGuy.TalkAboutYourself();
+            Console.WriteLine($"The tall guy has {tallGuy.FunnyThingIHave}");
+            tallGuy.Honk();
+
+            int[] values = new int[] { 0, 12, 44, 36, 92, 54, 13, 8 };
+            IEnumerable<int> result =
+                from v in values
+                where v < 37
+                orderby -v
+                select v;
+            IEnumerable<int> result2 = values.Where(v => v < 37).OrderByDescending(v => v);
+
+        }
+    }
+}
