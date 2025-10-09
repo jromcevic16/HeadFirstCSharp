fd4ffd2e345d8c51e85a14cee6b86224c1d7fc5d
jromcevic16@gmail.com <jromcevic16@gmail.com>
2025-10-09 14:45:23 +0200

    chapeter 9 cards linq

---
+++ Chapter9/CardLinq/CardLinq/Deck.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardsLinq
+{
+    internal class Deck
+    {
+        public Card[] cards = new Card[52];
+        Random r = new Random();
+        public Deck() 
+        {
+            int index = 0;
+            for (int suit = 0; suit <= 3; suit++)
+            {
+                for (int value = 1; value <= 13; value++)
+                {
+                    cards[index++] = new Card((Values)value, (Suits)suit);
+                }
+            }
+        }
+        public void PrintCards()
+        {
+            for (int i = 0; i < cards.Length; i++)
+            {
+                Console.WriteLine(cards[i].Name);
+            }
+        }
+        public Deck Shuffle()
+        {
+            for(int i = 0; i < cards.Length; i++)
+            {
+                int c2 = r.Next(cards.Length);
+                Card c = cards[i];
+                cards[i] = cards[c2];
+                cards[c2] = c;
+            }
+            return this;
+        }
+    }
+}
---
+++ Chapter9/CardLinq/CardLinq/Program.cs
@@ -0,0 +1,29 @@
+﻿using CardsLinq;
+
+namespace CardLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var deck = new Deck().Shuffle().cards.Take(16);
+            IOrderedEnumerable<IGrouping<Suits, Card>> groupd = groupBy(deck);
+            foreach (var group in groupd)
+            {
+                Console.WriteLine($@"Group: {group.Key}
+Count: {group.Count()}
+Minimum: {group.Min()}
+Maximum: {group.Max()}");
+            }
+            Console.WriteLine("Hello, World!");
+        }
+
+        private static IOrderedEnumerable<IGrouping<Suits, Card>> groupBy(IEnumerable<Card> deck)
+        {
+            return from card in deck
+                   group card by card.Suit into suitGroup
+                   orderby suitGroup.Key descending
+                   select suitGroup;
+        }
+    }
+}
